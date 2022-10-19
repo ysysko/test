00694409817f6e43b605f1800225c3b8d214a5b3
ysysko <yannik.hangst@syskomp-group.com>
2022-10-19 19:55:38 +0200

    test commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("TEST");
+
+let btn = document.querySelector("#test");
+
+btn.addEventListener("click", testFunc);
+
+function testFunc() {
+  return console.log("TEST");
+}
+
+const randomFunc = () => {
+  console.log("clicked");
+};
+
+// RANDOM TEST
